46d2eb5e470bd2c3973ea87b5803ee596dd7ffd2
Rick South <rick@natamus.com>
2023-12-12 00:26:30 +0100

    Quick Right-Click source code for 1.20.1.

---
+++ Common/src/main/java/com/natamus/quickrightclick/data/Constants.java
@@ -0,0 +1,15 @@
+package com.natamus.quickrightclick.data;
+
+import net.minecraft.network.chat.Component;
+
+public class Constants {
+    public static final Component CRAFTING_TABLE_TITLE = Component.translatable("container.crafting");
+    public static final Component CARTOGRAPHY_TABLE_TITLE = Component.translatable("container.cartography_table");
+    public static final Component ENDER_CHEST_TITLE = Component.translatable("container.enderchest");
+    public static final Component GRINDSTONE_TITLE = Component.translatable("container.grindstone_title");
+    public static final Component LOOM_TITLE = Component.translatable("container.loom");
+    public static final Component SHULKER_BOX_TITLE = Component.translatable("container.shulkerBox");
+    public static final Component SMITHING_TABLE_TITLE = Component.translatable("block.minecraft.smithing_table");
+    public static final Component STONECUTTER_TITLE = Component.translatable("container.stonecutter");
+    public static final String INVISIBLE_CHAR = "⠀ ";
+}
