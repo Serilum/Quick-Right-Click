42bbe18274e10c0025c07ddec02f53b53a93f13f
Rick South <rick@natamus.com>
2023-12-12 00:26:19 +0100

    Quick Right-Click source code for 1.19.2.

---
+++ Common/src/main/java/com/natamus/quickrightclick/menu/QuickCraftingTableMenu.java
@@ -0,0 +1,18 @@
+package com.natamus.quickrightclick.menu;
+
+import net.minecraft.world.entity.player.Inventory;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.inventory.ContainerLevelAccess;
+import net.minecraft.world.inventory.CraftingMenu;
+import org.jetbrains.annotations.NotNull;
+
+public class QuickCraftingTableMenu extends CraftingMenu {
+	public QuickCraftingTableMenu(int id, Inventory inventory, ContainerLevelAccess containerLevelAccess) {
+		super(id, inventory, containerLevelAccess);
+	}
+
+	@Override
+	public boolean stillValid(@NotNull Player player) {
+		return true;
+	}
+}
