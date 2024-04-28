1a5ba43fbc064cce4e63664861248e73771468d0
Rick South <rick@natamus.com>
2024-04-28 15:48:44 +0200

    Quick Right-Click source code for 1.20.5.

---
+++ Common/src/main/java/com/natamus/quickrightclick/util/Reference.java
@@ -0,0 +1,8 @@
+package com.natamus.quickrightclick.util;
+
+public class Reference {
+	public static final String MOD_ID = "quickrightclick";
+	public static final String NAME = "Quick Right-Click";
+	public static final String VERSION = "1.4";
+	public static final String ACCEPTED_VERSIONS = "[1.20.5]";
+}
