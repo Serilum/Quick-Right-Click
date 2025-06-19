c1a98b38cb0a59810120c65f8f4c91bd8c296be0
Rick South <rick@natamus.com>
2025-06-19 03:40:55 +0200

    Quick Right-Click source code for 1.21.6.

---
+++ NeoForge/src/main/java/com/natamus/quickrightclick/neoforge/events/NeoForgeQuickEvent.java
@@ -0,0 +1,12 @@
+package com.natamus.quickrightclick.neoforge.events;
+
+import com.natamus.quickrightclick.events.QuickEvent;
+import net.neoforged.neoforge.event.entity.player.PlayerInteractEvent;
+import net.neoforged.bus.api.SubscribeEvent;
+
+public class NeoForgeQuickEvent {
+	@SubscribeEvent
+	public static void onItemClick(PlayerInteractEvent.RightClickItem e) {
+		QuickEvent.onItemClick(e.getEntity(), e.getLevel(), e.getHand());
+	}
+}
